655a68cba29f938ae7983f69710943e2dd886e40
Alex <81694529+Clepfeed@users.noreply.github.com>
2024-09-12 11:04:46 +0300

    all proj xD

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+let NavUl = document.getElementById("nav");
+
+for(let i = 0; i < NavUl.childNodes.length; i++)
+{
+    if(NavUl.childNodes[i].localName == "li")
+    {
+        NavUl.childNodes[i].addEventListener("click", (event) => {
+            let ind = NavUl.childNodes[i].childNodes[0].href.indexOf("/pages");
+            if(ind == -1)
+            {
+                window.location.href = "/index.html";
+            }
+            else
+            {
+                window.location.href = NavUl.childNodes[i].childNodes[0].href.substr(ind);
+            }
+        });
+    }
+}
+
 $(document).on("click", aboutArrow, function(e) {
     e.preventDefault();
     let id  = $(this).attr('href');
